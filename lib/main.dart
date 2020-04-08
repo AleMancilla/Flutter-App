685af077880767f62068e59c3541e0b5456b5a77
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-07 23:39:09 -0400

    Hola Mundo desde una clase

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_flutter/src/hola.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
---
+++ lib/src/hola.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class myApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child:Text(
+          'Hello, Mari!',
+          textDirection: TextDirection.ltr
+          )
+          );
+  }
+
+}
