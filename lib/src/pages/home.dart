3d0e87fbcd902f909ecc6c46d7e9c47562fd57c9
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-09 00:13:45 -0400

    Floating acction button

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget{
+class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build 
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("hola", style: TextStyle(color: Colors.redAccent, backgroundColor: Colors.blueAccent),),
-        centerTitle: true,
-        backgroundColor: Colors.orange
+        appBar: AppBar(
+            title: Text(
+              "hola",
+              style: TextStyle(
+                  color: Colors.redAccent, backgroundColor: Colors.blueAccent),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.orange),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Hola mundo",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text("Hola Mari", style: TextStyle(fontSize: 30))
+            ],
+          ),
         ),
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Hola mundo", style: TextStyle(fontSize: 30),),
-          Text("Hola Mari", style: TextStyle(fontSize: 30))
-        ],
-      ),
-      )
-      
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+              print("Hola Mundo");
+            }, 
+          child: Icon(Icons.add),
+          ),
+        );
   }
 
+
+  fun(){
+    print("Hola Mundo");
+  }
 }
