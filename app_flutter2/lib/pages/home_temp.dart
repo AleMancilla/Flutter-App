6985e6d0dbcbe06a88fbc68b69b835c07628a18f
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-10 02:09:45 -0400

    devolver la lista en funcion

--- app_flutter2/lib/pages/home_temp.dart
+++ app_flutter2/lib/pages/home_temp.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 class HomeTemp extends StatelessWidget{
+
+  final datos = ["uno","dos","trs","cuatro"];
+
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(
         title: Text("PageTemp"),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(title: Text("TITULO"),),
-          Divider(),
-          ListTile(title: Text("TITULO"),),
-          Divider(),
-          ListTile(title: Text("TITULO"),),
-          Divider(),
-          ListTile(title: Text("TITULO"),),
-          Divider(),
-          
-        ],
+        children: _crear(),
       ),
     );
   }
+
+  List<Widget> _crear(){
+
+    List<Widget> lst = new List<Widget>();
+
+    for (var item in datos) {
+
+      final tempWidget = ListTile(title: Text(item));
+
+      lst..add(tempWidget)
+        ..add(Divider());
+    }
+
+    return lst;
+  } 
 
 }
