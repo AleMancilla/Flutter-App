1743649ee761d3cdc78a6ad3733e3cdc10d7e42b
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-14 01:01:59 -0400

    Circle Avatar

--- app_flutter2/lib/src/pages/Avatar.dart
+++ app_flutter2/lib/src/pages/Avatar.dart
@@ -5,7 +5,25 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text("AVATAR"),),
+      appBar: AppBar( title: Text("AVATAR"),
+      actions: <Widget>[
+        Container(
+          padding: EdgeInsets.all(5.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://dam.menshealthlatam.com/wp-content/uploads/2018/11/STANLEE.jpg'),
+            radius: 20.0,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(right: 10),
+          child: CircleAvatar(
+            child: Text("SL"),
+            backgroundColor: Colors.brown,
+            radius: 20.0,
+          ),
+        )
+      ],),
+      
     );
   }
 }
