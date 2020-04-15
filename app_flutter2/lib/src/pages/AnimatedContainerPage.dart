7ac83f5da6e0a557848378f1c33f43afdb540dcb
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-14 22:33:47 -0400

    container cubito

--- app_flutter2/lib/src/pages/AnimatedContainerPage.dart
+++ app_flutter2/lib/src/pages/AnimatedContainerPage.dart
@@ -8,10 +8,27 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+
+  double _height = 50.0;
+  double _width = 50.0;
+  Color _color = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("Hola"),
+    return Scaffold(
+      appBar: AppBar(title: Text("Animated Container"),),
+      body: Center(
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+        ),
+      ),
     );
   }
 }
