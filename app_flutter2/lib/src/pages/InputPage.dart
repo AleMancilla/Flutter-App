35b32f1550460563bd150143d676fba9cd5a1b72
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-15 01:54:13 -0400

    adicionamos correo contraseña y helper con label

--- app_flutter2/lib/src/pages/InputPage.dart
+++ app_flutter2/lib/src/pages/InputPage.dart
@@ -8,7 +8,8 @@ class InputPages extends StatefulWidget {
 
 class _InputPagesState extends State<InputPages> {
 
-  String _texto ;
+  String _texto ='' ;
+  String _correo = '';
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +19,12 @@ class _InputPagesState extends State<InputPages> {
         padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
         children: <Widget>[
         _inputfield(),
+        Divider(height: 5,),
+        _inputfieldCorreo(),
+        Divider(height: 5,),
+        _inputfieldPass(),
+        Divider(height: 5,),
+        _listaPersonas()
       ],),
     );
   }
@@ -25,18 +32,65 @@ class _InputPagesState extends State<InputPages> {
   _inputfield() {
     return TextField(
       decoration: InputDecoration(
-        icon: Icon(Icons.ac_unit),
-        prefixIcon: Icon(Icons.access_alarm),
+        icon: Icon(Icons.person),
         suffixIcon: Icon(Icons.accessibility),
         hintText: 'Hola Mundo mundial del mundo',
+        helperText: 'Datos',
+        labelText: 'label',
+        counterText: 'Longitud ${ _texto.length }',
         border: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(20.0))
         )
       ),
       onChanged: (variable){
+        setState(() {
         _texto = variable;
+        });
         print(_texto);
       },
+    );
+  }
+
+  
+  _inputfieldCorreo() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        icon: Icon(Icons.alternate_email),
+        suffixIcon: Icon(Icons.email),
+        hintText: 'Email',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        )
+      ),
+      onChanged: (variable){
+        setState(() {
+        _correo = variable;
+        });
+        print(_texto);
+      },
+    );
+  }
+  _inputfieldPass() {
+    return TextField(
+      obscureText: true,
+      keyboardType: TextInputType.visiblePassword,
+      decoration: InputDecoration(
+        icon: Icon(Icons.security),
+        suffixIcon: Icon(Icons.signal_cellular_no_sim),
+        hintText: 'Password',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        )
+      ),
+    );
+  }
+
+
+  Widget _listaPersonas(){
+    return ListTile(
+      title: Text("dato $_texto"),
+      subtitle: Text("Correo $_correo"),
     );
   }
 }
