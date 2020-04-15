d9a3dd997bc21f8eeb3d11a5c1d16fb4ad6603d2
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-14 21:57:53 -0400

    Nueva pagina y los datos en el json

--- app_flutter2/lib/src/rutas.dart
+++ app_flutter2/lib/src/rutas.dart
@@ -1,4 +1,5 @@
 import 'package:app_flutter2/src/pages/Alert.dart';
+import 'package:app_flutter2/src/pages/AnimatedContainerPage.dart';
 import 'package:app_flutter2/src/pages/Avatar.dart';
 import 'package:app_flutter2/src/pages/Cards.dart';
 import 'package:app_flutter2/src/pages/home_page.dart';
@@ -11,6 +12,7 @@ Map<String, WidgetBuilder> returnRouts(){
         'alert':(BuildContext context)=> AlertPage(),
         'avatar':(BuildContext context)=> AvatarPage(),
         'card':(BuildContext context)=> CardPage(),
+        'animateContainer':(BuildContext context)=> AnimatedContainerPage(),
       };
   }
 
--- app_flutter2/lib/src/utils/icon_string_util.dart
+++ app_flutter2/lib/src/utils/icon_string_util.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 final _iconos = <String, IconData>{
 "add_alert"       : Icons.add_alert,
 'accessibility'   : Icons.accessibility,
-'folder_open'     : Icons.folder_open
+'folder_open'     : Icons.folder_open,
+'donut_large'     : Icons.donut_large
 };
 Icon getIcon (String data){
   return Icon(_iconos[data], color: Colors.blue,) ;
