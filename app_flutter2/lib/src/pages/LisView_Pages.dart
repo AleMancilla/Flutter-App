ac70a13805c39a50a7fd56bf79b6a3e9676504a1
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-18 00:53:24 -0400

    ListView con imagenes

--- app_flutter2/lib/src/pages/LisView_Pages.dart
+++ app_flutter2/lib/src/pages/LisView_Pages.dart
@@ -8,12 +8,28 @@ class ListaPages extends StatefulWidget {
 }
 
 class _ListaPagesState extends State<ListaPages> {
+
+  List _images = [13,22,34,45,56,67,73,86,9];
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
          appBar: AppBar(title: Text("Listaaas"),),
+         body: _crearlista(),
        ),
     );
+  }
+
+  _crearlista() {
+    return ListView.builder(
+      itemCount: _images.length,
+      itemBuilder: (BuildContext context, int index){
+        final _img = _images[index];
+        return FadeInImage(
+          placeholder: AssetImage('recursos/jar_loading.gif'), 
+          image: NetworkImage('https://i.picsum.photos/id/$_img/600/600.jpg'));
+          
+      }
+      );
   }
 }
